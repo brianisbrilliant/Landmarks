4af3786713268180c462d727be826a2262bb1652
Brian Foster <brianisbrilliant@gmail.com>
2020-08-15 20:33:41 -0500

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Brian Foster on 8/15/20.
+//  Copyright © 2020 Brian Foster. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
